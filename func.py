fc2add0d1dd28f34c7a97f39c6e892991b622174
deboraslopes <debora.lopes@aluno.ifsertao-pe.edu.br>
2022-07-22 14:08:12 -0300

    treinando funções

--- func.py
+++ func.py
@@ -6,3 +6,11 @@ resultado = media(10, 8)
 print (resultado)
 
 
+def ant_e_suc(num):
+    ant = num - 1
+    suc = num + 1
+    return ant, suc
+
+antecessor, sucessor = ant_e_suc(5)
+print(antecessor)
+print(sucessor)
