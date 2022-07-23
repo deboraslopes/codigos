dec1e4650a3b9beefeac073499d17100d06feb3e
deboraslopes <debora.lopes@aluno.ifsertao-pe.edu.br>
2022-07-23 11:22:01 -0300

    create pratice

---
+++ praticando.py
@@ -0,0 +1,12 @@
+numeroCorreto = False
+while (numeroCorreto == False):
+   numero = int(input("Insira um número par"))
+   try:
+      numero = int(input())
+       if (numero%2 == 0):
+           numeroCorreto = True
+           print("Você digitou um numero par !")
+       else :
+           print("Você digitou um número impar")
+   except:
+       print("Caracter inválido, por favor digite um número par")
