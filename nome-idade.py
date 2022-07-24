77bf4446443b5c12b9fb67a0bd73b9ab01325721
deboraslopes <debora.lopes@aluno.ifsertao-pe.edu.br>
2022-07-24 01:14:04 -0300

    atualiza

--- nome-idade.py
+++ nome-idade.py
@@ -4,14 +4,23 @@
 
 anoCorreto = False
 while(anoCorreto == False):
-    ano = 
+    ano = input('Ano do nascimento: ')
+    try:
+     ano = int(input())
+     if (ano >=1922) and (ano <= 2021):
+          anoCorreto = True
+          print('Idade = ')
+     else:
+          print('Insira um ano de nascimento entre 1922 e 2021!')
+     except:
+          print('Caracter Inválido, por favor digite um número')
 
 numeroCorreto = False
 while (numeroCorreto == False):
-   numero = input("Insira um número par")
+   print("Insira um número par")
    try:
-      numero = int(input())
-      if (numero%2 == 0):
+       numero = int(input())
+       if (numero%2 == 0):
            numeroCorreto = True
            print("Você digitou um numero par !")
        else :
