80762b2d7b2400b7270d551ebdf1a60abcdc8cff
deboraslopes <debora.lopes@aluno.ifsertao-pe.edu.br>
2022-07-23 23:28:13 -0300

    Update nome-idade.py

--- nome-idade.py
+++ nome-idade.py
@@ -2,3 +2,19 @@
 
 #Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.
 
+anoCorreto = False
+while(anoCorreto == False):
+    ano = 
+
+numeroCorreto = False
+while (numeroCorreto == False):
+   numero = int(input("Insira um número par"))
+   try:
+      numero = int(input())
+       if (numero%2 == 0):
+           numeroCorreto = True
+           print("Você digitou um numero par !")
+       else :
+           print("Você digitou um número impar")
+   except:
+       print("Caracter inválido, por favor digite um número par")
