1ebdfcaae7b3c44f15873f807aec0400f3a9ba18
deboraslopes <debora.lopes@aluno.ifsertao-pe.edu.br>
2022-09-03 17:30:04 -0300

    atualiza

---
+++ JS/IMC/main.js
@@ -0,0 +1,15 @@
+function imc(){
+    var altura = parseFloat(document.getElementById("altura").value);
+    var peso = parseFloat(document.getElementById("peso").value);
+    var imc = peso/ (altura*2) ;
+    if(imc >= 18 && imc <= 25 )
+    if(imc==20)
+    alert("Parabens você está com o peso ideal");
+    if (imc < 18){
+    ("Cuidado! você precisa aumentar seu peso")
+}
+lse
+    alert ("Parabens, você está dentro da media”);
+    Else
+    alert("Cuidado! você precisa diminuir seu peso")}
+    
