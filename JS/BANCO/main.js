3862aa64b0dd09c20cf6302608b8c32fff4098e0
deboraslopes <debora.lopes@aluno.ifsertao-pe.edu.br>
2022-09-09 03:18:57 -0300

    atualiza

---
+++ JS/BANCO/main.js
@@ -0,0 +1,14 @@
+function banco(conta, saldo, tipo, ag){
+    this.conta = conta
+    this.saldo = saldo
+    this.tipo = tipo
+    this.ag = ag
+}
+
+banco.saldo = bucaSaldo
+
+var banco = {
+    saldo:function(){}
+}
+
+banco.saldo()
