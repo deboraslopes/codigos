170f9dc393e603782d273cf7db4019e3b2559808
deboraslopes <debora.lopes@aluno.ifsertao-pe.edu.br>
2022-09-09 02:14:34 -0300

    arrow

---
+++ JS/CALCULADORA2/dist/teste.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function funcCalc2(num1, num2) {
+  //função com parametros
+  return num1 + num2; //return
+}
+
+var funcCalc3 = function funcCalc3(a, b) {
+  return a + b;
+};
+
+funcCalc2(13, 22);
+funcCalc3(13, 23);
