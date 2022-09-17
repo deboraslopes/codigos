4c9a9523b15dae95a32662b3aeebf6177dc83eb1
deboraslopes <debora.lopes@aluno.ifsertao-pe.edu.br>
2022-09-17 10:33:27 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import express, { response } from 'express'
-import { request } from 'http'
-
+const { response } = require('express')
 const express = require ('express')
 
 const app = express()
 
-app.listen(3333)
+app.get('/', (require, response) => {
+    return response.send("Hello Word")
 
-app.get('/projects', request, response) => {
-    return response.send('Hello Word')
-}
+})
+
+
+app.listen(3333,() => {
+    console.log("Servidor iniciado na porta 3333: http://localhost:3333/ ")
+})
