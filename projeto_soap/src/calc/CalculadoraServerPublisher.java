f2700d35ed47c075844414b7b89b5e3ba67c1901
deboraslopes <debora.lopes@aluno.ifsertao-pe.edu.br>
2023-01-15 14:12:30 -0300

    update soap

--- projeto_soap/src/calc/CalculadoraServerPublisher.java
+++ projeto_soap/src/calc/CalculadoraServerPublisher.java
@@ -4,7 +4,7 @@ import javax.xml.ws.Endpoint;
 public class CalculadoraServerPublisher {
     public static void main(String[] args) {
         {
-            Endpoint.publish("http://192.168.0.1:9976/calc",
+            Endpoint.publish("http://192.168.0.1:8080/calc",
             new CalculadoraServerImpl());
         }
     }
